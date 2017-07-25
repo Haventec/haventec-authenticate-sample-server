c46dbb4a17dfd2ebc6f3ffe8d78525930ae6203a
John Kelleher <john.kelleher@haventec.com>
2017-07-25 16:50:04 +1000

    Removes h2o2 proxy

--- server.js
+++ server.js
@@ -67,178 +67,150 @@ server.route({
     }
 });
 
-server.register({
-    register: require('h2o2')
-}, function (err) {
 
-    if (err) {
-        console.log('Failed to load h2o2');
-    }
+// Returns a registration token for a self-service created user
+server.route({
+    method: 'POST',
+    path: '/self-service/user',
+    handler: function (request, reply) {
+        console.info('Called POST self-service/user');
 
-    // Returns a registration token for a self-service created user
-    server.route({
-        method: 'POST',
-        path: '/self-service/user',
-        handler: {
-            proxy: {
-                passThrough: true,
-                mapUri: function (request, callback) {
-                    let uri = haventecServerURL + request.url.path;
-                    console.info('Called POST /self-service/user');
-                    callback(null, uri, globalHeaders);
-                },
-                onResponse: function (err, res, request, reply) {
-
-                    // Get the payload from the http.IncomingMessage obj
-                    Wreck.read(res, null, function (err, payload) {
-                        let data = JSON.parse(payload.toString());
-
-                        if(data.status !== undefined){
-                            console.log(data.status);
-                        }
-
-                        // Add the headers from the upstream
-                        reply(payload).headers = res.headers;
-                    });
-                }
+        var  body = {
+            "accessToken":{
+                "token":"",
+                "type":""
+            },
+            "applicationUuid":"",
+            "authKey":"",
+            "deviceUuid":"",
+            "userUuid":"",
+            "responseStatus":{
+                "message":"",
+                "code":"",
+                "status":"SUCCESS"
             }
-        }
-    });
+        };
 
-    // Registers the user and returns their new authentication keys and JWT
-    server.route({
-        method: 'POST',
-        path: '/register/user',
-        handler: function (request, reply) {
-            console.info('Called POST /register/user');
+        reply(body);
+    }
+});
 
-            var  body = {
-                "accessToken":{
-                    "token":"",
-                    "type":""
-                },
-                "applicationUuid":"",
-                "authKey":"",
-                "deviceUuid":"",
-                "userUuid":"",
-                "responseStatus":{
-                    "message":"",
-                    "code":"",
-                    "status":"SUCCESS"
-                }
-            };
+// Registers the user and returns their new authentication keys and JWT
+server.route({
+    method: 'POST',
+    path: '/register/user',
+    handler: function (request, reply) {
+        console.info('Called POST /register/user');
 
-            reply(body);
-        }
-    });
+        var  body = {
+            "accessToken":{
+                "token":"",
+                "type":""
+            },
+            "applicationUuid":"",
+            "authKey":"",
+            "deviceUuid":"",
+            "userUuid":"",
+            "responseStatus":{
+                "message":"",
+                "code":"",
+                "status":"SUCCESS"
+            }
+        };
 
-    // Logs the user in and returns a new set of authentication keys and JWT
-    server.route({
-        method: 'POST',
-        path: '/login',
-        handler: function (request, reply) {
-            console.info('Called POST /login');
+        reply(body);
+    }
+});
 
-            var  body = {
-                "accessToken": {
-                    "token": "string",
-                    "type": "string"
-                },
-                "authKey": "string",
-                "responseStatus":{
-                    "message":"",
-                    "code":"",
-                    "status":"SUCCESS"
-                }
-            };
+// Logs the user in and returns a new set of authentication keys and JWT
+server.route({
+    method: 'POST',
+    path: '/login',
+    handler: function (request, reply) {
+        console.info('Called POST /login');
 
-            reply(body);
-        }
-    });
+        var  body = {
+            "accessToken": {
+                "token": "string",
+                "type": "string"
+            },
+            "authKey": "string",
+            "responseStatus":{
+                "message":"",
+                "code":"",
+                "status":"SUCCESS"
+            }
+        };
 
-    // Logs the user out and invalidates their JWT
-    server.route({
-        method: 'DELETE',
-        path: '/logout',
-        handler: function (request, reply) {
-            console.info('Called DELETE /logout');
+        reply(body);
+    }
+});
 
-            var  body = {
-                "responseStatus":{
-                    "message":"",
-                    "code":"",
-                    "status":"SUCCESS"
-                }
-            };
+// Logs the user out and invalidates their JWT
+server.route({
+    method: 'DELETE',
+    path: '/logout',
+    handler: function (request, reply) {
+        console.info('Called DELETE /logout');
 
-            reply(body);
-        }
-    });
+        var  body = {
+            "responseStatus":{
+                "message":"",
+                "code":"",
+                "status":"SUCCESS"
+            }
+        };
 
-    // Calls forgot pin for a given user and returns a reset token
-    server.route({
-        method: 'POST',
-        path: '/forgot-pin',
-        handler: function (request, reply) {
-            console.info('Called POST /forgot-pin');
+        reply(body);
+    }
+});
 
-            var  body = {
-                "resetToken":"",
-                "responseStatus":{
-                    "message":"",
-                    "code":"",
-                    "status":"SUCCESS"
-                }
-            };
+// Calls forgot pin for a given user and returns a reset token
+server.route({
+    method: 'POST',
+    path: '/forgot-pin',
+    handler: function (request, reply) {
+        console.info('Called POST /forgot-pin');
 
-            reply(body);
-        }
-    });
+        var  body = {
+            "resetToken":"",
+            "responseStatus":{
+                "message":"",
+                "code":"",
+                "status":"SUCCESS"
+            }
+        };
 
-    // Resets the user's PIN and returns their new authentication keys and JWT
-    server.route({
-        method: 'POST',
-        path: '/reset-pin',
-        handler: function (request, reply) {
-            console.info('Called POST /reset-pin');
+        reply(body);
+    }
+});
 
-            var  body =     {
-                "deviceUuid":"",
-                "accessToken":{
-                    "type":"",
-                    "token":""
-                },
-                "authKey":"",
-                "responseStatus":{
-                    "message":"",
-                    "code":"",
-                    "status":"SUCCESS"
-                }
-            };
+// Resets the user's PIN and returns their new authentication keys and JWT
+server.route({
+    method: 'POST',
+    path: '/reset-pin',
+    handler: function (request, reply) {
+        console.info('Called POST /reset-pin');
 
-            reply(body);
-        }
-    });
+        var  body =     {
+            "deviceUuid":"",
+            "accessToken":{
+                "type":"",
+                "token":""
+            },
+            "authKey":"",
+            "responseStatus":{
+                "message":"",
+                "code":"",
+                "status":"SUCCESS"
+            }
+        };
 
-    // server.route({
-    //     method: 'GET',
-    //     path: '/{p*}',
-    //     handler: {
-    //         proxy: {
-    //             mapUri: function (request, callback) {
-    //                 var headers = globalHeaders;
-    //                 var uri = haventecServerURL + request.url.path;
-    //                 callback(null, uri, headers);
-    //             },
-    //             onResponse: function (err, res, request, reply) {
-    //                 reply(res).header('X-Res-Header', 'I\'m a custom response header');
-    //             }
-    //         }
-    //     }
-    // });
+        reply(body);
+    }
+});
 
-    server.start(function (err) {
-        console.log('Haventec Authenticate sample server started at: ' + server.info.uri);
-        console.log('This server is NOT intended to be used in a Production environment.');
-    });
+server.start(function (err) {
+    console.log('Haventec Authenticate sample server started at: ' + server.info.uri);
+    console.log('This server is NOT intended to be used in a Production environment.');
 });
