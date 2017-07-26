d48c862414b820edc44e321e2c44db61ab8e6b1d
justincrosbie <justin.crosbie@journeytree.com.au>
2017-07-27 08:45:21 +1000

    SMTP Settings

--- config.js
+++ config.js
@@ -8,18 +8,18 @@ config.mail = {};
 config.application.apiKey = 'your-api-key';
 
 // The Haventec Authenticate Sever domain
-config.application.haventecSever = 'https://anbe-dev.aws.haventec.com';
+config.application.haventecSever = 'https://api.haventec.com';
 
 // The host this server is to use, e.g localhost
 // config.server.host = (process.env.IP || 'localhost');
 config.server.port = (process.env.PORT || 5000);
 
 // The mail server details
-config.mail.host = 'smtp.example.com';
-config.mail.port = 456;
+config.mail.host = 'smtp.gmail.com';
+config.mail.port = 587;
 config.mail.secure = false;
-config.mail.username = 'username@example.com';
-config.mail.password = 'password';
-config.mail.fromAddress = '"Name" <no-reply@example.com>';
+config.mail.username = 'smtp.relay@haventec.com';
+config.mail.password = 'q*Xyr9nh*';
+config.mail.fromAddress = '"Haventec" <no-reply@haventec.com>';
 
 module.exports = config;
