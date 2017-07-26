cb1b5d672c801541580897539055d12b2544b3e0
justincrosbie <justin.crosbie@journeytree.com.au>
2017-07-26 11:51:56 +1000

    Sets port

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ config.application.haventecSever = 'https://anbe-dev.aws.haventec.com';
 
 // The host this server is to use, e.g localhost
 config.server.host = 'localhost';
-config.server.port = '5000';
+config.server.port = (process.env.PORT || 5000);
 
 // The mail server details
 config.mail.host = 'smtp.example.com';
