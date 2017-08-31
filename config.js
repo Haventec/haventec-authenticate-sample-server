c89e1813489d2b26ee7764d8a0f88b3fdfe406a9
justincrosbie <justin.crosbie@journeytree.com.au>
2017-08-31 22:08:40 +1000

    JC changes

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ config.server = {};
 config.mail = {};
 
 // This is your Haventec API key that is provided by Haventec Cloud Portal, https://cloudportal.haventec.com
-config.application.apiKey = '53382e68-6d0b-4c71-ace2-197a8d82698f';
+config.application.apiKey = '038e5228-668f-463c-ad17-8a65a196239a';
 
 // The Haventec Authenticate Server domain
-config.application.haventecServer = 'api.haventec.com';
+config.application.haventecServer = 'localhost';
 
 // The host this server is to use, e.g localhost
 // config.server.host = (process.env.IP || 'localhost');
