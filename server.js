a2aceba1eb42928921a14deadd62471f49416902
John Kelleher <john.kelleher@haventec.com>
2017-12-07 15:53:51 +1100

    Adds the logout api - adds more logs

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ server.register(require('inert'), (err) => {
         method: 'POST',
         path: '/activate/user',
         handler: function (request, reply) {
-            console.info('Called POST /activate/user');
+            console.info('\nCalled POST /activate/user');
             callHaventecServer('/authenticate/v1-2/authentication/activate/user', 'POST', request.payload, function (result) {
                 reply(result);
             }, reply);
@@ -70,7 +70,7 @@ server.register(require('inert'), (err) => {
         method: 'POST',
         path: '/activate/device',
         handler: function (request, reply) {
-            console.info('Called POST /activate/device');
+            console.info('\nCalled POST /activate/device');
             callHaventecServer('/authenticate/v1-2/authentication/activate/device', 'POST', request.payload, function (result) {
                 reply(result);
             }, reply);
@@ -82,7 +82,7 @@ server.register(require('inert'), (err) => {
         method: 'POST',
         path: '/login',
         handler: function (request, reply) {
-            console.info('Called POST /login');
+            console.info('\nCalled POST /login');
             callHaventecServer('/authenticate/v1-2/authentication/login', 'POST', request.payload, function (result) {
                 reply(result);
             }, reply);
@@ -94,10 +94,10 @@ server.register(require('inert'), (err) => {
         method: 'DELETE',
         path: '/logout',
         handler: function (request, reply) {
-            console.info('Called DELETE /logout');
-            callHaventecServer('/authenticate/v1-2/authentication/logout', 'DELETE', request.payload, function (result) {
+            console.info('\nCalled DELETE /logout');
+            callHaventecServer('/authenticate/v1-2/authentication/logout', 'DELETE', '', function (result) {
                 reply(result);
-            }, reply);
+            }, reply, request);
         }
     });
 
@@ -106,7 +106,7 @@ server.register(require('inert'), (err) => {
         method: 'POST',
         path: '/reset-pin',
         handler: function (request, reply) {
-            console.info('Called POST /reset-pin');
+            console.info('\nCalled POST /reset-pin');
             callHaventecServer('/authenticate/v1-2/authentication/reset-pin', 'POST', request.payload, function (result) {
                 reply(result);
             }, reply);
@@ -118,7 +118,7 @@ server.register(require('inert'), (err) => {
         method: 'GET',
         path: '/user/current',
         handler: function (request, reply) {
-            console.info('Called Get /user/current');
+            console.info('\nCalled Get /user/current');
             callHaventecServer('/authenticate/v1-2/user/current', 'GET', '', function (result) {
                 reply(result);
             }, reply, request);
@@ -130,7 +130,7 @@ server.register(require('inert'), (err) => {
         method: 'POST',
         path: '/device',
         handler: function (request, reply) {
-            console.info('Called POST /self-service/device');
+            console.info('\nCalled POST /self-service/device');
 
             callHaventecServer('/authenticate/v1-2/self-service/device', 'POST', request.payload, function (result) {
                 if (result.activationToken !== undefined && result.userEmail !== undefined) {
@@ -151,7 +151,7 @@ server.register(require('inert'), (err) => {
         method: 'POST',
         path: '/forgot-pin',
         handler: function (request, reply) {
-            console.info('Called POST /forgot-pin');
+            console.info('\nCalled POST /forgot-pin');
 
             callHaventecServer('/authenticate/v1-2/authentication/forgot-pin', 'POST', request.payload, function (result) {
                 if (result.resetPinToken !== undefined && result.userEmail !== undefined) {
@@ -172,7 +172,7 @@ server.register(require('inert'), (err) => {
         method: 'POST',
         path: '/self-service/user',
         handler: function (request, reply) {
-            console.info('Called POST /self-service/user');
+            console.info('\nCalled POST /self-service/user');
             callHaventecServer('/authenticate/v1-2/self-service/user', 'POST', request.payload, function (result) {
                 reply(result);
             }, reply);
@@ -184,7 +184,7 @@ server.register(require('inert'), (err) => {
         method: 'GET',
         path: '/test-email',
         handler: function (request, reply) {
-            console.info('Called POST /test-email');
+            console.info('\nCalled POST /test-email');
 
             let params = request.query;
             let result = 'Test failed: no email or invalid email address was supplied';
@@ -201,8 +201,8 @@ server.register(require('inert'), (err) => {
     });
 
     server.start(function (err) {
-        console.log('Haventec Authenticate sample server started at: ' + server.info.uri);
-        console.log('This server is NOT intended to be used in a Production environment.');
+        console.info('Haventec Authenticate sample server started at: ' + server.info.uri);
+        console.info('This server is NOT intended to be used in a Production environment.');
     });
 
 });
@@ -216,15 +216,15 @@ function sendEmail(email, subject, body){
     };
 
     if(config.mail.host === ''){
-        return console.log('Mail server is not configured');
+        return console.info('Mail server is not configured');
     }
     else {
         transporter.sendMail(mailOptions, (error, info) => {
             if (info) {
-                return console.log('Mail server info', info);
+                return console.info('Mail server info', info);
             }
             if (error) {
-                return console.log("Send mail", error);
+                return console.warn("Send mail", error);
             }
         });
     }
@@ -233,25 +233,29 @@ function sendEmail(email, subject, body){
 function callHaventecServer(path, method, payload, callback, reply, request) {
     const authenticateUrl = 'https://' + config.application.haventecServer + path;
 
-    console.log('Authenticate URL: ', authenticateUrl );
+    console.info('Authenticate URL: ', authenticateUrl );
 
     http(method, authenticateUrl)
-    .send(payload)
-    .set(setHeaders(request))
-    .then(
-        (res) => {callback(res.body)},
-        (err) => {
-            console.log("ERROR:", err.message);
-            reply({responseStatus: {status: "ERROR", message: err.message, code: ""}});
-        }
-    );
+        .set(setHeaders(request))
+        .send(payload)
+        .then(
+            (res) => {
+                console.info("SUCCESS:\n", res.body);
+                callback(res.body)},
+            (err) => {
+                console.warn("ERROR:", err.message);
+                reply({responseStatus: {status: "ERROR", message: err.message, code: ""}});
+            }
+        );
 }
 
 function setHeaders(request) {
     if(((((request || {}).raw || {}).req || {}).headers || {}).authorization) {
         let headers = _.clone(globalHeaders);
         headers['Authorization'] = request.raw.req.headers.authorization;
+        console.info('Sending Headers\n', headers);
         return headers;
     }
+    console.info('Sending Global Headers\n', globalHeaders);
     return globalHeaders;
 }
